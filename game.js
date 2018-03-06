bb49b74f8a97724d66ae55d45284a5a766d5748a
mrassett <magsrassett@gmail.com>
2018-03-06 17:15:04 -0600

    A bunch of commented-out code

--- game.js
+++ game.js
@@ -1,49 +1,19 @@
 "use strict"
 
-
-// function randLetter( ) {
-//     let letters = ("abcdefghijklmnopqrstuvwxyz");
-//     let psy = "";
-//     let i = 0;
-//     while (i < randLetter){
-//         psy += letters.indexOf(Math.floor(Math.random() * letters.length));
-//         i++;
-//     }
-//     return psy;
-// }
-
-// let randStr = "abcdefghijklmnopqrstuvwxyz";
-// document.write("Computer Picks Random Letter", randStr.charAt(Math.random) + "<br>");
-
-
 // let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 // 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 // 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// let letters = "abcdefghijklmnopqrstuvwxyz";
-// let randomLetter  = letters.charCodeAt(Math.floor(Math.random() * 26));
+//The computer selects a random letter//
 
-// document.write("letter")  + randomLetter;
-
-
-
-
-
-
-
-// supposedly checks if a value is inside of an array
-// function inArray(arrayToCheck, value){
-//     for(i = 0; i < arrayToCheck.length; i++){
-//         if(arrayToCheck[i] === value){
-//             return true;
-//         }
-//     }
-//     return false;
-// }
-
-// let randArray = [1,2,3,4,5];
-
-// document.write("In Array: ", inArray(randArray, 4));
+// let selector = Math.floor(Math.random() * letters.length);
+// let randomLetter = letters[selector];
+// console.log(randomLetter);
+// let wins = 0;
+// let losses = 0;
+// let turnsLeft = 9;
+// let guessesGuessed = [ ];
+// let alphaSelected = null;
 
 
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
@@ -60,14 +30,48 @@ let turnsLeft = turns.length - 1;
 
 
 function guess() {
- if (guess == selector); {
-     alert("Win")
+ if (guess === selector); {
+     document.write ("Win", "<br>")
     }
 if (guess != selector);{
-    alert("Lose")
+    document.write("Lose")
 }
 }
 
-//  (randomLetter.valueOf != selector); {
-//      document.write("lose")
-//  }
+//The compuer asses the guess, and alters the number of turns left, or displays the word "winner" if correct//
+
+// Non-working code
+// document.onkeypress = function gamer(){
+//     let alphaSelected = String.fromCharCode(event.keyCode);
+
+// if (guessesGuessed[guessesGuessed.length] = alphaSelected);
+
+// return document.getElementById("letter").innerHTML = turnsLeft - 1;
+// }
+
+//  _____________________________
+
+// document.onkeypress = function gamer(){
+//     let guess = Event.keypress; 
+//     if (guess === randomLetter) {
+//             let turnsLeft = turnsLeft - 1;
+//             return document.getElementById("letter").innerHTML = "Win";
+//             }
+// }
+//  _________________________________
+
+// // This will display the word "win" no matter what
+
+
+
+//use below
+// function guess () { 
+//  while (turnsLeft < 10);
+//  if (guess === selector); {
+//      document.write("Win");
+//     }
+// if (guess != selector);{
+//     return document.getElementById("losses").innerHTML = "Lose";
+// }
+// }
+
